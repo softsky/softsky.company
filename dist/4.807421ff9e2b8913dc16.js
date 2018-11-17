719de286a4046d797ca8e3a7197fc0243113bc17
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-11-17 12:42:05 +0200

    Sat Nov 17 12:42:05 EET 2018

---
+++ dist/4.807421ff9e2b8913dc16.js
@@ -0,0 +1,297 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  [4],
+  {
+    OlGY: function(l, n, b) {
+      'use strict';
+      b.r(n);
+      var a = b('CcnG'),
+        u = (function() {
+          return function() {};
+        })(),
+        t = b('t68o'),
+        c = b('zbXB'),
+        A = b('NcP4'),
+        e = b('xYTU'),
+        o = b('pMnS'),
+        i = b('lzlj'),
+        r = b('FVSy'),
+        s = b('A7o+'),
+        d = b('Mr+X'),
+        p = b('SMsm'),
+        f = b('AytR'),
+        m = (function() {
+          function l() {
+            this.version = f.a.version;
+          }
+          return (l.prototype.ngOnInit = function() {}), l;
+        })(),
+        g = a.rb({
+          encapsulation: 0,
+          styles: [
+            [
+              '.container[_ngcontent-%COMP%]{text-align:center;padding:1rem}.mat-icon[_ngcontent-%COMP%]{vertical-align:middle}'
+            ]
+          ],
+          data: {}
+        });
+      function h(l) {
+        return a.Kb(
+          0,
+          [
+            (l()(), a.tb(0, 0, null, null, 25, 'div', [['class', 'container']], null, null, null, null, null)),
+            (l()(), a.Ib(-1, null, ['\n  '])),
+            (l()(), a.tb(2, 0, null, null, 22, 'mat-card', [['class', 'mat-card']], null, null, null, i.b, i.a)),
+            a.sb(3, 49152, null, 0, r.a, [], null, null),
+            (l()(), a.Ib(-1, 0, ['\n    '])),
+            (l()(), a.tb(5, 0, null, 0, 6, 'h1', [], null, null, null, null, null)),
+            (l()(), a.Ib(-1, null, ['\n      '])),
+            (l()(),
+            a.tb(
+              7,
+              0,
+              null,
+              null,
+              3,
+              'mat-card-title',
+              [['class', 'mat-card-title'], ['translate', '']],
+              null,
+              null,
+              null,
+              null,
+              null
+            )),
+            a.sb(8, 8536064, null, 0, s.e, [s.l, a.k, a.h], { translate: [0, 'translate'] }, null),
+            a.sb(9, 16384, null, 0, r.g, [], null, null),
+            (l()(), a.Ib(-1, null, ['APP_NAME'])),
+            (l()(), a.Ib(-1, null, ['\n    '])),
+            (l()(), a.Ib(-1, 0, ['\n    '])),
+            (l()(),
+            a.tb(
+              13,
+              0,
+              null,
+              0,
+              10,
+              'mat-card-content',
+              [['class', 'mat-card-content']],
+              null,
+              null,
+              null,
+              null,
+              null
+            )),
+            a.sb(14, 16384, null, 0, r.c, [], null, null),
+            (l()(), a.Ib(-1, null, ['\n      '])),
+            (l()(),
+            a.tb(
+              16,
+              0,
+              null,
+              null,
+              2,
+              'mat-icon',
+              [['class', 'mat-icon'], ['role', 'img']],
+              [[2, 'mat-icon-inline', null]],
+              null,
+              null,
+              d.b,
+              d.a
+            )),
+            a.sb(17, 9158656, null, 0, p.b, [a.k, p.d, [8, null], [2, p.a]], null, null),
+            (l()(), a.Ib(-1, 0, ['code'])),
+            (l()(), a.Ib(-1, null, ['\n      '])),
+            (l()(), a.tb(20, 0, null, null, 2, 'span', [['translate', '']], null, null, null, null, null)),
+            a.sb(21, 8536064, null, 0, s.e, [s.l, a.k, a.h], { translate: [0, 'translate'] }, null),
+            (l()(), a.Ib(-1, null, ['Version'])),
+            (l()(), a.Ib(23, null, [' ', '\n    '])),
+            (l()(), a.Ib(-1, 0, ['\n  '])),
+            (l()(), a.Ib(-1, null, ['\n'])),
+            (l()(), a.Ib(-1, null, ['\n']))
+          ],
+          function(l, n) {
+            l(n, 8, 0, ''), l(n, 17, 0), l(n, 21, 0, '');
+          },
+          function(l, n) {
+            var b = n.component;
+            l(n, 16, 0, a.Cb(n, 17).inline), l(n, 23, 0, b.version);
+          }
+        );
+      }
+      function v(l) {
+        return a.Kb(
+          0,
+          [
+            (l()(), a.tb(0, 0, null, null, 1, 'app-about', [], null, null, null, h, g)),
+            a.sb(1, 114688, null, 0, m, [], null, null)
+          ],
+          function(l, n) {
+            l(n, 1, 0);
+          },
+          null
+        );
+      }
+      var I = a.pb('app-about', m, v, {}, {}, []),
+        w = b('Ip0R'),
+        j = b('OzfB'),
+        k = b('eDkP'),
+        y = b('Fzqc'),
+        M = b('4tE/'),
+        O = b('M2Lx'),
+        P = b('Wf4p'),
+        z = b('o3x0'),
+        C = b('jQLj'),
+        x = b('mVsa'),
+        L = b('dWZg'),
+        Y = b('uGex'),
+        S = b('v9Dh'),
+        q = b('ZYjt'),
+        B = b('4epT'),
+        D = b('OkvK'),
+        K = b('wmQ5'),
+        V = b('21Lb'),
+        Z = b('hUWP'),
+        G = b('3pJQ'),
+        N = b('V9q+'),
+        Q = b('4c35'),
+        R = b('qAlS'),
+        W = b('UodH'),
+        F = b('u7R8'),
+        J = b('de3e'),
+        U = b('/dO6'),
+        X = b('lLAP'),
+        _ = b('LC5p'),
+        E = b('YhbO'),
+        H = b('jlZm'),
+        T = b('seP3'),
+        $ = b('r43C'),
+        ll = b('/VYK'),
+        nl = b('b716'),
+        bl = b('0/Q6'),
+        al = b('Z+uX'),
+        ul = b('Blfk'),
+        tl = b('9It4'),
+        cl = b('Nsh5'),
+        Al = b('kWGw'),
+        el = b('w+lc'),
+        ol = b('vARd'),
+        il = b('Lwpp'),
+        rl = b('y4qS'),
+        sl = b('BHnd'),
+        dl = b('La40'),
+        pl = b('8mMr'),
+        fl = b('vvyD'),
+        ml = b('ZYCi'),
+        gl = b('ey9i'),
+        hl = { title: Object(gl.c)('About') },
+        vl = (function() {
+          return function() {};
+        })(),
+        Il = b('YSh2');
+      b.d(n, 'AboutModuleNgFactory', function() {
+        return wl;
+      });
+      var wl = a.qb(u, [], function(l) {
+        return a.zb([
+          a.Ab(512, a.j, a.fb, [[8, [t.a, c.b, c.a, A.a, e.a, e.b, o.a, I]], [3, a.j], a.z]),
+          a.Ab(4608, w.m, w.l, [a.w, [2, w.w]]),
+          a.Ab(4608, j.j, j.i, [j.d, j.g]),
+          a.Ab(
+            5120,
+            a.b,
+            function(l, n) {
+              return [j.m(l, n)];
+            },
+            [w.d, a.D]
+          ),
+          a.Ab(4608, k.a, k.a, [k.g, k.c, a.j, k.f, k.d, a.s, a.B, w.d, y.b, [2, w.g]]),
+          a.Ab(5120, k.h, k.i, [k.a]),
+          a.Ab(5120, M.a, M.b, [k.a]),
+          a.Ab(4608, O.c, O.c, []),
+          a.Ab(4608, P.d, P.d, []),
+          a.Ab(5120, z.b, z.c, [k.a]),
+          a.Ab(135680, z.d, z.d, [k.a, a.s, [2, w.g], [2, z.a], z.b, [3, z.d], k.c]),
+          a.Ab(4608, C.h, C.h, []),
+          a.Ab(5120, C.a, C.b, [k.a]),
+          a.Ab(5120, x.b, x.g, [k.a]),
+          a.Ab(4608, P.c, P.y, [[2, P.h], L.a]),
+          a.Ab(5120, Y.a, Y.b, [k.a]),
+          a.Ab(5120, S.a, S.b, [k.a]),
+          a.Ab(4608, q.f, P.e, [[2, P.i], [2, P.n]]),
+          a.Ab(5120, B.b, B.a, [[3, B.b]]),
+          a.Ab(5120, D.b, D.a, [[3, D.b]]),
+          a.Ab(5120, K.b, K.a, [[3, K.b]]),
+          a.Ab(1073742336, w.c, w.c, []),
+          a.Ab(1073742336, s.i, s.i, []),
+          a.Ab(1073742336, j.e, j.e, []),
+          a.Ab(1073742336, y.a, y.a, []),
+          a.Ab(1073742336, V.c, V.c, []),
+          a.Ab(1073742336, Z.b, Z.b, []),
+          a.Ab(1073742336, G.a, G.a, []),
+          a.Ab(1073742336, N.a, N.a, [[2, j.k], a.D]),
+          a.Ab(1073742336, P.n, P.n, [[2, P.f], [2, q.g]]),
+          a.Ab(1073742336, L.b, L.b, []),
+          a.Ab(1073742336, P.x, P.x, []),
+          a.Ab(1073742336, P.v, P.v, []),
+          a.Ab(1073742336, P.s, P.s, []),
+          a.Ab(1073742336, Q.g, Q.g, []),
+          a.Ab(1073742336, R.c, R.c, []),
+          a.Ab(1073742336, k.e, k.e, []),
+          a.Ab(1073742336, M.c, M.c, []),
+          a.Ab(1073742336, W.c, W.c, []),
+          a.Ab(1073742336, F.a, F.a, []),
+          a.Ab(1073742336, r.e, r.e, []),
+          a.Ab(1073742336, O.d, O.d, []),
+          a.Ab(1073742336, J.a, J.a, []),
+          a.Ab(1073742336, U.b, U.b, []),
+          a.Ab(1073742336, z.i, z.i, []),
+          a.Ab(1073742336, X.a, X.a, []),
+          a.Ab(1073742336, C.i, C.i, []),
+          a.Ab(1073742336, _.b, _.b, []),
+          a.Ab(1073742336, E.c, E.c, []),
+          a.Ab(1073742336, H.a, H.a, []),
+          a.Ab(1073742336, T.e, T.e, []),
+          a.Ab(1073742336, P.o, P.o, []),
+          a.Ab(1073742336, $.a, $.a, []),
+          a.Ab(1073742336, p.c, p.c, []),
+          a.Ab(1073742336, ll.c, ll.c, []),
+          a.Ab(1073742336, nl.b, nl.b, []),
+          a.Ab(1073742336, bl.c, bl.c, []),
+          a.Ab(1073742336, x.e, x.e, []),
+          a.Ab(1073742336, P.z, P.z, []),
+          a.Ab(1073742336, P.p, P.p, []),
+          a.Ab(1073742336, Y.c, Y.c, []),
+          a.Ab(1073742336, S.c, S.c, []),
+          a.Ab(1073742336, B.c, B.c, []),
+          a.Ab(1073742336, al.a, al.a, []),
+          a.Ab(1073742336, ul.c, ul.c, []),
+          a.Ab(1073742336, tl.a, tl.a, []),
+          a.Ab(1073742336, cl.h, cl.h, []),
+          a.Ab(1073742336, Al.c, Al.c, []),
+          a.Ab(1073742336, el.a, el.a, []),
+          a.Ab(1073742336, ol.d, ol.d, []),
+          a.Ab(1073742336, D.c, D.c, []),
+          a.Ab(1073742336, il.d, il.d, []),
+          a.Ab(1073742336, K.c, K.c, []),
+          a.Ab(1073742336, rl.o, rl.o, []),
+          a.Ab(1073742336, sl.a, sl.a, []),
+          a.Ab(1073742336, dl.a, dl.a, []),
+          a.Ab(1073742336, pl.b, pl.b, []),
+          a.Ab(1073742336, fl.a, fl.a, []),
+          a.Ab(1073742336, ml.p, ml.p, [[2, ml.v], [2, ml.l]]),
+          a.Ab(1073742336, vl, vl, []),
+          a.Ab(1073742336, u, u, []),
+          a.Ab(256, U.a, { separatorKeyCodes: [Il.f] }, []),
+          a.Ab(256, P.g, P.k, []),
+          a.Ab(
+            1024,
+            ml.j,
+            function() {
+              return [[{ path: '', component: m, data: hl }]];
+            },
+            []
+          )
+        ]);
+      });
+    }
+  }
+]);
