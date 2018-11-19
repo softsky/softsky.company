ccdc541ec0eb1921cf7a1eb2a7d39ca18c719b4b
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-11-19 15:56:09 +0200

    AUTH_CONFIG added to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,18 @@ export const environment = {
   supportedLanguages: [
     'en-US',
     'fr-FR'
-  ]
+  ],
+  AUTH_CONFIG : {
+    backend: {
+      clientID: 'FIhO7vNId2Al1wdfKJDhBuY4NwGZLB5i',
+      domain: 'softsky.eu.auth0.com',
+      redirectUri: 'http://avalache.github.io/callback',
+      scope: 'openid',
+      responseType: 'token id_token'
+    },
+    storage: localStorage,
+    storageKey: 'currentUser',
+    publicRoute: ['public'],
+    defaultUrl: ''
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -13,12 +13,21 @@ export const environment = {
   supportedLanguages: [
     'en-US',
     'fr-FR'
-  ]
+  ],
+  AUTH_CONFIG : {
+    backend: {
+      clientID: 'FIhO7vNId2Al1wdfKJDhBuY4NwGZLB5i',
+      domain: 'softsky.eu.auth0.com',
+      redirectUri: 'http://localhost:3000/callback',
+      scope: 'openid',
+      responseType: 'token id_token'
+    },
+    storage: localStorage,
+    storageKey: 'currentUser',
+    publicRoute: ['public'],
+    defaultUrl: ''
+  }
 };
-
-export const AUTH_CONFIG = {
-
-}
 
 /*
  * For easier debugging in development mode, you can import the following file
