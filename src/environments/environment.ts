a9b6640b609cd043b2653dd6abde8e238bee621e
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-11-20 11:24:40 +0200

    AUTH_CONFIG added to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,21 @@ export const environment = {
   version: env.npm_package_version + '-dev',
   serverUrl: '/api',
   defaultLanguage: 'en-US',
-  supportedLanguages: ['en-US', 'fr-FR']
+  supportedLanguages: ['en-US', 'fr-FR'],
+
+  AUTH_CONFIG: {
+    backend: {
+      clientID: 'FIhO7vNId2Al1wdfKJDhBuY4NwGZLB5i',
+      domain: 'softsky.eu.auth0.com',
+      redirectUri: 'http://localhost:4200/auth-callback',
+      scope: 'openid',
+      responseType: 'token id_token'
+    },
+    storage: localStorage,
+    storageKey: 'currentUser',
+    publicRoute: ['public'],
+    defaultUrl: ''
+  }
 };
 
 /*
