88236bba16c3bf9386f1f5af438d9ceda8253bf0
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-11-22 11:11:40 +0200

    now use EMPTY_LABELS and EMPTY_DATA, improved error handling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Observable, fromEvent, of, from } from 'rxjs';
-import { map, scan, reduce } from 'rxjs/operators';
+import { Observable, fromEvent, of, from, throwError } from 'rxjs';
+import { map, scan, reduce, catchError } from 'rxjs/operators';
 
 import { finalize } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 import { Logger } from '@app/core';
 
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { LineChartComponent } from '../line-chart/line-chart.component';
+import { EMPTY_LABELS, EMPTY_DATA, LineChartComponent } from '../line-chart/line-chart.component';
 
 import * as moment from 'moment';
 
@@ -49,9 +49,10 @@ export class HomeComponent implements OnInit {
     this.route.fragment.subscribe((fragment: string) => {
       this.emailForm.controls.email.setValue(fragment);
     });
+
     document.querySelectorAll('button.open-next').forEach((e, idx) =>
       fromEvent(e, 'click')
-        .pipe(map(it => this.state++))
+        .pipe(map(it => (this.state = idx + 1)))
         .subscribe(it => log.debug('Debug', this.state))
     );
     // fetch data
@@ -60,61 +61,71 @@ export class HomeComponent implements OnInit {
 
   initChart(event: LineChartComponent) {
     if (['', null, undefined].indexOf(this.emailForm.controls.email.value) > -1) return; // leaving if value is empty
-    const o = this.http.get('https://haveibeenpwned.com/api/v2/breachedaccount/' + this.emailForm.controls.email.value);
+    const o = this.http
+      .get('https://haveibeenpwned.com/api/v2/breachedaccount/' + this.emailForm.controls.email.value)
+      .pipe(
+        catchError(error => {
+          if (this.chart && this.chart.baseChart && this.chart.baseChart.chart && this.chart.baseChart.chart.config) {
+            this.chart.lineChartLabels = EMPTY_LABELS;
+            this.chart.lineChartData = EMPTY_DATA;
+            this.chart.baseChart.chart.update();
+          }
+          if (error.error instanceof ErrorEvent) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.error('An error occurred:', error.error.message);
+          } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong,
+            console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+          } // return an observable with a user-facing error message
+          return throwError('Something bad happened; please try again later.');
+        })
+      );
 
-    o.subscribe(
-      (arr: Array<any>) => {
-        this.breachedSites = arr;
-        this.breachedCount = this.breachedSites.length;
+    o.subscribe((arr: Array<any>) => {
+      this.breachedSites = arr;
+      this.breachedCount = this.breachedSites.length;
 
-        const breachedByDate: Array<any> = arr
-          .map((it: any) => {
-            return { date: new Date(it.BreachDate), size: it.PwnCount };
-          })
-          .sort((a: any, b: any) => {
-            if (a.date > b.date) {
-              return 1;
-            }
-            if (a.date < b.date) {
-              return -1;
-            }
-            return 0;
-          });
+      const breachedByDate: Array<any> = arr
+        .map((it: any) => {
+          return { date: new Date(it.BreachDate), size: it.PwnCount };
+        })
+        .sort((a: any, b: any) => {
+          if (a.date > b.date) {
+            return 1;
+          }
+          if (a.date < b.date) {
+            return -1;
+          }
+          return 0;
+        });
 
-        const researchedByDate: Array<any> = arr
-          .map((it: any) => {
-            return { date: new Date(it.AddedDate), size: it.PwnCount };
-          })
-          .sort((a: any, b: any) => {
-            if (a.date > b.date) {
-              return 1;
-            }
-            if (a.date < b.date) {
-              return -1;
-            }
-            return 0;
-          });
+      const researchedByDate: Array<any> = arr
+        .map((it: any) => {
+          return { date: new Date(it.AddedDate), size: it.PwnCount };
+        })
+        .sort((a: any, b: any) => {
+          if (a.date > b.date) {
+            return 1;
+          }
+          if (a.date < b.date) {
+            return -1;
+          }
+          return 0;
+        });
 
-        this.chart.lineChartData = [
-          { data: breachedByDate.map(it => it.size), label: 'Breached' },
-          { data: researchedByDate.map(it => it.size), label: 'Researched' }
-        ];
-        this.chart.lineChartLabels = breachedByDate.map(it => moment(it.date).format('MM/YYYY'));
-        this.breachedCount = breachedByDate.length;
+      this.chart.lineChartData = [
+        { data: breachedByDate.map(it => it.size), label: 'Breached' },
+        { data: researchedByDate.map(it => it.size), label: 'Researched' }
+      ];
+      this.chart.lineChartLabels = breachedByDate.map(it => moment(it.date).format('MM/YYYY'));
+      this.breachedCount = breachedByDate.length;
 
-        if (this.chart && this.chart.baseChart && this.chart.baseChart.chart && this.chart.baseChart.chart.config) {
-          this.chart.baseChart.chart.config.data.labels = this.chart.lineChartLabels;
-          this.chart.baseChart.chart.update();
-        }
-      },
-      err => {
-        // 404
-        if (this.chart && this.chart.baseChart && this.chart.baseChart.chart && this.chart.baseChart.chart.config) {
-          this.chart.lineChartData = [];
-          this.chart.baseChart.chart.update();
-        }
+      if (this.chart && this.chart.baseChart && this.chart.baseChart.chart && this.chart.baseChart.chart.config) {
+        this.chart.baseChart.chart.config.data.labels = this.chart.lineChartLabels;
+        this.chart.baseChart.chart.update();
       }
-    );
+    });
 
     // Draw chart
   }
@@ -138,10 +149,11 @@ export class HomeComponent implements OnInit {
           a.appendChild(document.createTextNode('Click to send first email'));
           a.href = msg.ReceiveMessageResponse.ReceiveMessageResult.messages[0].Body;
           a.target = '_new';
-          e.target.parentElement.parentElement.appendChild(a);
-          var evt = document.createEvent('MouseEvents');
-          evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-          a.dispatchEvent(evt);
+          a.click();
+          //e.target.parentElement.parentElement.appendChild(a);
+          // var evt = document.createEvent('MouseEvents');
+          // evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+          // a.dispatchEvent(evt);
           // you can check allowDefault for false to see if
           // any handler called evt.preventDefault().
           // Firefox will *not* redirect to anchorObj.href
