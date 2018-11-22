9f7d13cbe2bce6083ef64752282216faa987743c
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2018-11-22 12:16:21 +0200

    fixed colors for chart

--- src/app/line-chart/line-chart.component.ts
+++ src/app/line-chart/line-chart.component.ts
@@ -33,21 +33,21 @@ export class LineChartComponent implements OnInit {
   public lineChartColors: Array<any> = [
     {
       // grey
-      backgroundColor: 'rgba(148,159,177,0.2)',
-      borderColor: 'rgba(148,159,177,1)',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
+      backgroundColor: 'rgba(204,0,0,0.2)',
+      borderColor: 'rgba(204,0,0,1)',
+      pointBackgroundColor: 'rgba(204,0,0,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+      pointHoverBorderColor: 'rgba(204,0,0,0.8)'
     },
     {
       // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
+      backgroundColor: 'rgba(0,102,102,0.2)',
+      borderColor: 'rgba(0,102,102,1)',
+      pointBackgroundColor: 'rgba(0,102,102,1)',
       pointBorderColor: '#fff',
       pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
+      pointHoverBorderColor: 'rgba(0,102,102,1)'
     }
   ];
 
